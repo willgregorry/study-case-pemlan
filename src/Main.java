d4e7a7e869d70b2008c81275215f12d20fc57770
wilzzzzzzzz <itswgxx@gmail.com>
2025-05-09 10:20:55 +0700

    refactor: move main method to MainFrame

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        Authentication user = new Authentication("keiza", "keiza503an");
-        user.register(user.getUsername(), user.getPassword());
-        user.login(user.getUsername(), user.getPassword());
-
-//        SwingUtilities.invokeLater(MainFrame::new);
-
-        System.out.println();
-        System.out.println("Fayas test pull");
+        SwingUtilities.invokeLater(MainFrame::new);
     }
 }
